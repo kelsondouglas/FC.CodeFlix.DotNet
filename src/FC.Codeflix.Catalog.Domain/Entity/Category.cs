45baf593f636e3b38d06642fae333bf5b2ede0e9
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-02-01 12:27:26 -0300

    aula 8: refatorando com SeedWork

--- src/FC.Codeflix.Catalog.Domain/Entity/Category.cs
+++ src/FC.Codeflix.Catalog.Domain/Entity/Category.cs
@@ -1,22 +1,21 @@
 ﻿
 using FC.CodeFlix.Catalog.Domain.Exceptions;
+using FC.CodeFlix.Catalog.Domain.SeedWork;
 
 namespace FC.CodeFlix.Catalog.Domain.Entity;
 
 //Classe Public
-public class Category
+public class Category : AggregateRoot
 {
     //Declaração de atributos da classe
-    public Guid Id { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
     public bool IsActive { get; private set; }
     public DateTime CreatedAt { get; private set; }
 
     //Declarando construtor da Classe
-    public Category(string name, string description, bool isActive = true)
+    public Category(string name, string description, bool isActive = true) : base()
     {
-        Id = Guid.NewGuid();
         Name = name;
         Description = description;
         IsActive = isActive;
